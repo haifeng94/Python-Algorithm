b8b2583286d87102609404d697b41a8cea66e277
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-03-08 19:24:52 +0800

    leetcode

--- leetcode/hard/t1.py
+++ leetcode/hard/t1.py
@@ -32,7 +32,7 @@ class Solution:
 class Solution:
     def maxNumber(self, nums1, nums2, k):
         for i in range(k+1):
-            if i <= len(nums1) and k-i <= len(nums2): #nums1从小到大遍历，nums2从大到小遍历。获取两个列表里每个列表所有可能提供的值的个数
+            if i <= len(nums1) and k-i <= len(nums2): #nums1从小到大遍历，nums2从大到小遍历。从两个列表获取k个值
                 p1 = self.prep(nums1, i)
                 p2 = self.prep(nums2, k-i)
                 self.merge(p1, p2)
