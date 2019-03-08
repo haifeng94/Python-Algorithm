af843c3aecf2fd5258f051d3479b7adb9afe833b
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-03-08 19:22:03 +0800

    leetcode

--- leetcode/hard/t1.py
+++ leetcode/hard/t1.py
@@ -6,6 +6,27 @@ Given two arrays of length m and n with digits 0-9 representing two numbers. Cre
 The relative order of the digits from the same array must be preserved. Return an array of the k digits.
 
 Note: You should try to optimize your time and space complexity.
+
+solution：
+
+class Solution:
+    def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
+        def prep(nums, k):
+            drop = len(nums) - k
+            out = []
+            for num in nums:
+                while drop and out and out[-1] < num:
+                    out.pop()
+                    drop -= 1
+                out.append(num)
+            return out[:k]
+
+        def merge(a, b):
+            return [max(a, b).pop(0) for _ in a+b]
+
+        return max(merge(prep(nums1, i), prep(nums2, k-i))
+                   for i in range(k+1)
+                   if i <= len(nums1) and k-i <= len(nums2))
 '''
 
 class Solution:
