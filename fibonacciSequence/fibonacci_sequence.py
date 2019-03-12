d0365fdba398679dfb28df338836a99ff9ee0d11
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-03-12 17:28:16 +0800

    Python3实现斐波那契数列

---
+++ fibonacciSequence/fibonacci_sequence.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+class Fibonacci(object):
+    def fibonacci_recursion_tool(self, n):
+        '''递归实现'''
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fibonacci_recursion_tool(n-1) + self.fibonacci_recursion_tool(n-2)
+
+    def fibonacci_recursion(self, n):
+        res = []
+        for i in range(1, n+1):
+            res.append(self.fibonacci_recursion_tool(i))
+        print(res)
+        return res
+
+    def fibonacci_loop_tool(self, n):
+        '''循环实现'''
+        a, b = 0, 1
+        while n > 0:
+            a, b = b, a+b
+            n -= 1
+
+    def fibonacci_loop(self, n):
+        res = []
+        a, b = 0, 1
+        while n > 0:
+            res.append(b)
+            a, b = b, a+b
+            n -= 1
+        print(res)
+        return res
+
+    def fibonacci_yield_tool(self, n):
+        '''通过关键字yield优化循环实现'''
+        a, b = 0, 1
+        while n > 0:
+            yield b
+            a, b = b, a+b
+            n -= 1
+
+    def fibonacci_yield(self, n):
+        return list(self.fibonacci_yield_tool(n))
+
+if __name__ == "__main__":
+    f = Fibonacci()
+    f.fibonacci_recursion(10)
+    f.fibonacci_loop(12)
+    y = f.fibonacci_yield(15)
+    print(y)
