cf7f9a60019e4b98fa2749bce77404f54ac8f91d
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-03-23 18:51:48 +0800

    二叉搜索树的后序遍历序列

---
+++ 剑指offer/解决面试题思路/举例让抽象问题具体化/PostOrder.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+'''
+题目：输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。
+假设输入的数组的任意两个数字都互不相同。
+'''
+
+'''
+二叉搜索树即是二叉排序树，
+1. 后序遍历序列的最后一个元素为二叉树的根节点；
+2. 二叉搜索树左子树上所有的结点均小于根结点、右子树所有的结点均大于根结点。
+算法步骤如下：
+1. 找到根结点；
+2. 遍历序列，找到第一个大于等于根结点的元素i，则i左侧为左子树、i右侧为右子树；
+3. 我们已经知道i左侧所有元素均小于根结点，那么再依次遍历右侧，看是否所有元素均大于根结点；若出现小于根结点的元素，则直接返回false；若右侧全都大于根结点，则：
+4. 分别递归判断左/右子序列是否为后序序列；
+
+'''
+
+def is_post_order(order):
+    length = len(order)
+    if length:
+        root = order[-1]
+        left = 0
+        while order[left] < root:
+            left += 1
+        right = left
+        while right < length - 1:
+            if order[right] < root:
+                return False
+            right += 1
+        left_res = True if left == 0 else is_post_order(order[:left])
+        right_res = True if left == right else is_post_order(order[left:right])
+        return left_res and right_res
+    return False
